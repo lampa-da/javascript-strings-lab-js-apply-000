3c3966708d2c2eec4c21b3d227ae4d56f10a682e
Ekaterina Svetlakova <radistkatya@gmail.com>
2017-12-09 03:13:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let's talk about undefined ${topic}`;
+var conversation = `Let's talk about ${topic}.`;
